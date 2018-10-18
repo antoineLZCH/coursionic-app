e4732f9088410dbc0a3c53646f6d4bd657021f38
Antoine <antoine.lozach@imie.fr>
2018-10-18 15:45:40 +0200

    initial commit

---
+++ src/pages/login/login.ts
@@ -0,0 +1,80 @@
+import { Component } from '@angular/core';
+import { TranslateService, USE_STORE } from '@ngx-translate/core';
+import { IonicPage, NavController, MenuController } from 'ionic-angular';
+import { UserProvider } from "../../providers/user/user";
+import { HttpProvider } from "../../providers/http/http";
+import { User } from "../../models/user";
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html'
+})
+
+/**
+ * @author: KMR
+ * @email: yajuve.25.dz@gmail.com
+ */
+
+export class LoginPage {
+  // Permet de pré-remplir les formulaires d'inscription
+  // et de connexion.
+  public account: User = {
+    "password": "alzch",
+    "username": "anLZCH",
+    "avatar": "Raouf.png",
+    "fullname": "Antoine LZCH",
+    "email": "antoine.lozach@imie.fr"
+  };
+
+  // Our translated text strings
+  public loginErrorString: string; // Message d'erreur à la connexion
+  private opt: string = 'signin'; // définition de la tab par défaut
+
+  constructor(public http: HttpProvider, public userProvider: UserProvider, public menuCtrl: MenuController, public navCtrl: NavController, public translateService: TranslateService) {
+    // Pas de Menu
+    this.menuCtrl.enable(false);
+    // Promesse de traduction (multithreading)
+    this.translateService.get('LOGIN_ERROR').subscribe((value) => {
+      this.loginErrorString = value;
+    })
+  }
+
+  // Attempt to login in through our User service
+  doLoginv1() {
+    this.http.get('my-profile.json').subscribe((profile: User) => {
+      // Requête async dans le fichier my-profile.json,
+      // renvoie le résultat dans la variable profile
+      this.userProvider.user = <User>profile;
+      if (this.checkConnection(profile))
+        this.navCtrl.setRoot('ListFriendsPage');
+      else {
+        this.account.email = ""
+        this.account.password = ""
+        this.loginErrorString = "LOGIN ERROR"
+      }
+      // supprime toutes les vues de la stack et définir une nouvelle rootPage
+      // navCtrl = navigation
+    }, (err) => {
+      console.error(err);
+    });
+  }
+
+  doLogin() {
+    this.userProvider.loginUser(this.account.email, this.account.password).then(
+      isConnect => {
+        if (isConnect)
+          this.navCtrl.setRoot('ListFriendsPage')
+        else
+          this.loginErrorString = 'Connection error'
+      }
+    )
+  }
+
+  doRegister() {
+    this.userProvider.registerUser(this.account);
+  }
+  checkConnection(user: User) {
+    return (user.email === this.account.email && user.password === this.account.password)
+  }
+}
---
+++ src/providers/user/user.ts
@@ -0,0 +1,141 @@
+import { Injectable } from '@angular/core';
+import { User } from "../../models/user";
+import { Storage } from '@ionic/storage';
+
+/*
+ Generated class for the UserProvider provider.
+
+ See https://angular.io/guide/dependency-injection for more info on providers
+ and Angular DI.
+ */
+@Injectable()
+export class UserProvider {
+
+  private _user: User = new User();
+  private _status: Number = 0;
+
+  constructor(public ionStorage: Storage) {
+    console.log('Hello UserProvider Provider');
+    this.statusUser().then(
+      status => this._status = status
+    )
+  }
+
+
+  get user(): User {
+    return this._user;
+  }
+
+  set user(value: User) {
+    this._user = value;
+  }
+
+  statusUser() {
+    return this.ionStorage.get('user')
+      .then(
+        data => {
+          if (data == null) {
+            this.ionStorage.set('users', [])
+            return 0
+          } else {
+            if (Array.isArray(data)) {
+              return (data.length > 0) ? 1 : -1;
+            }
+            else {
+              this.ionStorage.set('users', [])
+              return 0
+            }
+          }
+        },
+        error => {
+          this.ionStorage.set('users', [])
+          return 0
+        }
+      )
+  }
+
+  checkedEmail(email: String) {
+    return this.ionStorage.get('users').then(
+      users => {
+        for (let i = 0; i < users.length; i++) {
+          if (users[i].email === email) {
+            return true;
+          }
+        }
+        return false;
+      }
+    )
+  }
+
+  loginUser(mail: String, pwd: String) {
+    return this.checkedEmail(mail).then(
+      data => {
+        if (data) {
+          this.ionStorage.get('users').then(
+            data => {
+              for (let i = 0; i < data.length; i++)
+                if (data[i].email === mail && data[i].password === pwd)
+                  return data[i];
+              return false;
+            }
+          )
+        }
+        return false;
+      }
+    )
+  }
+
+  registerUser(user: User) {
+    switch (this._status) {
+      case 1:
+        return this.ionStorage.get('users')
+          .then(
+            users => {
+              let isValid: boolean = false
+              for (let i = 0; i < users.length; i++)
+                if (users[i].email === user.email)
+                  isValid = true
+              if (isValid)
+                return false
+              users.push(user)
+              return true
+            }
+          )
+        break;
+      case 0:
+      case -1:
+        return this.ionStorage.set('users', [user]).then(
+          data => {
+            return true;
+          }
+        )
+        break;
+      default:
+        return false;
+    }
+  }
+
+  updateUser(user: User, isEmail: any = { type: false }) {
+    return this.ionStorage.get('users').then(
+      users => {
+        for (let i = 0; i < users.length; i++) {
+          if (users[i].email === user.email) {
+            this._user = user;
+            users[i] = user;
+            this.ionStorage.set('users', users)
+            return true
+          }
+          if (isEmail) {
+            if (users[i].email === isEmail.email) {
+              this._user = user;
+              users[i] = user;
+              this.ionStorage.set('users', users)
+              return true
+            }
+          }
+          return false
+        }
+    )
+  }
+
+}
